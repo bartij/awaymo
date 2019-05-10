1b9c17b97259c6861649c039ff0fa27f862e5355
Bartosz Jurga <barti.jurga@gmail.com>
2019-05-10 19:38:01 +0200

    project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MediaQuery from 'react-responsive';
 
-function App() {
+import AppWrapper from './AppWrapper'
+
+const DEVICE_BREAKPOINT = 992;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrapper>
+      <MediaQuery maxWidth={DEVICE_BREAKPOINT - 1}>
+        mobile
+      </MediaQuery>
+      <MediaQuery minWidth={DEVICE_BREAKPOINT}>
+        desktop
+      </MediaQuery>
+    </AppWrapper>
   );
 }
 
---
+++ src/AppWrapper.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+const AppWrapper = styled.div`
+  background-color: #EE5F63;
+  padding: 0 10px;
+`;
+
+export default AppWrapper
